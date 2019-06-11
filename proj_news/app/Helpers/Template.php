a9b3dc8af155b288124f06abf38c4db938124a01
vudinhquang <vudinhquangk53@gmail.com>
2019-06-12 03:35:32 +0900

    showButtonAction - show ra danh sach button

--- proj_news/app/Helpers/Template.php
+++ proj_news/app/Helpers/Template.php
@@ -24,4 +24,31 @@ class Template {
         $xhtml = sprintf('<img src="%s" alt="%s" class="zvn-thumb">', asset("images/$controllerName/$thumbName"), $thumbAlt);
         return $xhtml;
     }
+
+    public static function showButtonAction ($controllerName, $id) {
+        $tmplButton = [
+            'edit'   => ['class' => 'btn-success', 'title' => 'Edit', 'icon' => 'fa-pencil', 'route-name' => $controllerName.'/form'],
+            'delete' => ['class' => 'btn-danger', 'title' => 'Delete', 'icon' => 'fa-trash', 'route-name' => $controllerName.'/delete'],
+            'info'   => ['class' => 'btn-info', 'title' => 'View', 'icon' => 'fa-pencil', 'route-name' => $controllerName.'/delete']
+        ];
+        $buttonInArea = [
+            'default' => ['edit', 'delete'],
+            'slider'  =>  ['edit', 'delete']
+        ];
+
+        $controllerName = (array_key_exists($controllerName, $buttonInArea)) ? $controllerName : 'default';
+        $listButtons    = $buttonInArea[$controllerName]; 
+        $xhtml          = '<div class="zvn-box-btn-filter">';
+        foreach ($listButtons as $key => $btn) {
+            $currentButton = $tmplButton[$btn];
+            $link = route($currentButton['route-name'], ['id' => $id]);
+            $xhtml .= sprintf('<a href="%s" type="button" class="btn btn-icon %s" data-toggle="tooltip" data-placement="top" 
+                                data-original-title="%s">
+                                <i class="fa %s"></i>
+                        </a>', $link, $currentButton['class'], $currentButton['title'], $currentButton['icon']);
+        }
+        $xhtml .= '</div>';
+
+        return $xhtml;
+    }
 }
