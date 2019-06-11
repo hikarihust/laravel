fbfae4915a3e8b5d100c074c044cc05747f34341
vudinhquang <vudinhquangk53@gmail.com>
2019-06-12 02:15:50 +0900

    showItemThumb - Xu ly phan hien thi cho tam hinh

--- proj_news/app/Helpers/Template.php
+++ proj_news/app/Helpers/Template.php
@@ -2,13 +2,13 @@
 namespace App\Helpers;
 use Config;
 class Template {
-    public static function showItemHistory($by, $time) {
+    public static function showItemHistory ($by, $time) {
         $xhtml = sprintf('<p><i class="fa fa-user"></i> %s</p>
                     <p><i class="fa fa-clock-o"></i> %s</p>', $by, date(Config::get('zvn.format.short_time'), strtotime($time)));
         return $xhtml;
     }
 
-    public static function showItemStatus($controllerName, $id, $status) {
+    public static function showItemStatus ($controllerName, $id, $status) {
         $tmpStatus = [
             'active' => ['name' => 'Kích hoạt', 'class' => 'btn-success'],
             'inactive' => ['name' => 'Chưa kích hoạt', 'class' => 'btn-info']
@@ -17,6 +17,11 @@ class Template {
         $link          = route($controllerName.'/status', ['status' => $status, 'id' => $id]);
         $xhtml = sprintf('<a href="%s" type="button" class="btn btn-round %s">%s</a>', $link, $currentStatus['class'], $currentStatus['name']);
 
+        return $xhtml;
+    }
+
+    public static function showItemThumb ($controllerName, $thumbName, $thumbAlt) {
+        $xhtml = sprintf('<img src="%s" alt="%s" class="zvn-thumb">', asset("images/$controllerName/$thumbName"), $thumbAlt);
         return $xhtml;
     }
 }
