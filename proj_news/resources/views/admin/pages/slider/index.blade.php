e7083af9b5082d73356ad231a83bff3b20f02e30
vudinhquang <vudinhquangk53@gmail.com>
2019-06-15 01:15:38 +0900

    custom view - custom lai giao dien phan tao link phan trang

--- proj_news/resources/views/admin/pages/slider/index.blade.php
+++ proj_news/resources/views/admin/pages/slider/index.blade.php
@@ -87,7 +87,7 @@
     <div class="col-md-12 col-sm-12 col-xs-12">
       <div class="x_panel">
         @include('admin.templates.x_title', ['title' => 'Phân trang'])
-        @include('admin.templates.pagigation')
+        @include('admin.templates.pagination')
       </div>
     </div>
   </div>
