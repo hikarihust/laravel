c05add1870fdc1d629e9a1966557ff4238ac0ead
vudinhquang <vudinhquangk53@gmail.com>
2019-06-22 18:07:42 +0900

    Highlight - Highlight tu khoa tim kiem o cac ket qua tim duoc, tim theo truong nao thi highlight o truong do

--- proj_news/resources/views/admin/pages/slider/list.blade.php
+++ proj_news/resources/views/admin/pages/slider/list.blade.php
@@ -1,5 +1,6 @@
 @php
     use App\Helpers\Template as Template;
+    use App\Helpers\Highlight as Highlight;
 @endphp
 
 <div class="x_content">
@@ -22,9 +23,9 @@
               $index       = $key + 1;
               $class       = ($index%2 === 0) ? 'even' : 'odd';
               $id          = $val->id;
-              $name        = $val->name;
-              $description = $val->description;
-              $link        = $val->link;
+              $name        = Highlight::show($val->name, $params['search'], 'name');
+              $description = Highlight::show($val->description, $params['search'], 'description');
+              $link        = Highlight::show($val->link, $params['search'], 'link');
               $thumb       = Template::showItemThumb($controllerName, $val->thumb, $val->name);
               $status      = Template::showItemStatus($controllerName, $id, $val->status);
               $createdHistory   = Template::showItemHistory($val->created_by, $val->created);
@@ -34,9 +35,9 @@
             <tr class="{{ $class }} pointer">
             <td> {{ $index }} </td>
               <td width="40%">
-                <p><strong>Name:</strong> {{ $name }} </p>
-                <p><strong>Description:</strong>{{ $description }}</p>
-                <p><strong>Link:</strong>{{ $link }}</p>
+                <p><strong>Name:</strong> {!! $name !!} </p>
+                <p><strong>Description:</strong>{!! $description !!}</p>
+                <p><strong>Link:</strong>{!! $link !!}</p>
                 <p>{!! $thumb !!}</p>
               </td>
               <td>{!! $status !!}</td>
