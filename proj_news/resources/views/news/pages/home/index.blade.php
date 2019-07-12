933b995a5eebe8fe40a8f169696107656bd54b4f
vudinhquang <vudinhquangk53@gmail.com>
2019-07-13 05:52:58 +0900

    fix image - Sửa các lỗi liên quan đến đường dẫn hình ảnh, tách phần hiển thị nội dung trang index thành các block

--- proj_news/resources/views/news/pages/home/index.blade.php
+++ proj_news/resources/views/news/pages/home/index.blade.php
@@ -2,5 +2,33 @@
 @section('content')
 	<!-- Home -->
 	@include('news.block.slider')
-	quang
+	<!-- Content Container -->
+	<div class="content_container">
+		<div class="container">
+			<div class="row">
+				<!-- Main Content -->
+				<div class="col-lg-9">
+					<div class="main_content">
+						<!-- Featured -->
+						@include('news.block.featured', ['itemsFeatured' => array()])
+						<!-- Category -->
+						@include('news.pages.home.child-index.category')
+					</div>
+				</div>
+				<!-- Sidebar -->
+				<div class="col-lg-3">
+					<div class="sidebar">
+						<!-- Latest Posts -->
+						@include('news.block.latest_posts', ['itemsLatest' => array()])
+						<!-- Advertisement -->
+						@include('news.block.advertisement', ['itemsAdvertisement' => array()])
+						<!-- Most Viewed -->
+						@include('news.block.most_viewed', ['itemsMostViewed' => array()])
+						<!-- Tags -->
+						@include('news.block.tags', ['itemsTags' => array()])
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
